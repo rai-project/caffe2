5140c77b03aa693801d108097b04c7ad0627e572
Cheng Li <cli99@illinois.edu>
2017-09-28 00:34:14 -0500

    add tracing

--- predict/predict.go
+++ predict/predict.go
@@ -8,6 +8,7 @@ import (
 	context "golang.org/x/net/context"
 
 	opentracing "github.com/opentracing/opentracing-go"
+	olog "github.com/opentracing/opentracing-go/log"
 	"github.com/pkg/errors"
 	"github.com/rai-project/caffe2"
 	"github.com/rai-project/config"
@@ -17,6 +18,7 @@ import (
 	"github.com/rai-project/downloadmanager"
 	gocaffe2 "github.com/rai-project/go-caffe2"
 	"github.com/rai-project/image/types"
+	"github.com/rai-project/tracer/ctimer"
 )
 
 type ImagePredictor struct {
@@ -64,6 +66,7 @@ func (p *ImagePredictor) Load(ctx context.Context, model dlframework.ModelManife
 				Framework:         framework,
 				Model:             model,
 				PredictionOptions: opts,
+				Tracer:            tracer,
 			},
 			WorkDir: workDir,
 		},
@@ -105,37 +108,40 @@ func (p *ImagePredictor) GetPreprocessOptions(ctx context.Context) (common.Prepr
 }
 
 func (p *ImagePredictor) download(ctx context.Context) error {
-	if span, newCtx := opentracing.StartSpanFromContext(
+	span, ctx := p.GetTracer().StartSpanFromContext(
 		ctx,
 		"Download",
 		opentracing.Tags{
 			"graph_url":           p.GetGraphUrl(),
-			"traget_graph_file":   p.GetGraphPath(),
+			"target_graph_file":   p.GetGraphPath(),
 			"weights_url":         p.GetWeightsUrl(),
-			"traget_weights_file": p.GetWeightsPath(),
+			"target_weights_file": p.GetWeightsPath(),
 			"feature_url":         p.GetFeaturesUrl(),
-			"traget_feature_file": p.GetFeaturesPath(),
+			"target_feature_file": p.GetFeaturesPath(),
 		},
-	); span != nil {
-		ctx = newCtx
-		defer span.Finish()
-	}
+	)
+	defer span.Finish()
 
 	model := p.Model
 	if model.Model.IsArchive {
 		baseURL := model.Model.BaseUrl
+		span.LogFields(
+			olog.String("event", "download model archive"),
+		)
 		_, err := downloadmanager.DownloadInto(baseURL, p.WorkDir, downloadmanager.Context(ctx))
 		if err != nil {
 			return errors.Wrapf(err, "failed to download model archive from %v", model.Model.BaseUrl)
 		}
 		return nil
 	}
-
 	checksum := p.GetGraphChecksum()
 	if checksum == "" {
 		return errors.New("Need graph file checksum in the model manifest")
 	}
 
+	span.LogFields(
+		olog.String("event", "download graph"),
+	)
 	if _, err := downloadmanager.DownloadFile(p.GetGraphUrl(), p.GetGraphPath(), downloadmanager.MD5Sum(checksum)); err != nil {
 		return err
 	}
@@ -145,6 +151,9 @@ func (p *ImagePredictor) download(ctx context.Context) error {
 		return errors.New("Need weights file checksum in the model manifest")
 	}
 
+	span.LogFields(
+		olog.String("event", "download weights"),
+	)
 	if _, err := downloadmanager.DownloadFile(p.GetWeightsUrl(), p.GetWeightsPath(), downloadmanager.MD5Sum(checksum)); err != nil {
 		return err
 	}
@@ -154,6 +163,9 @@ func (p *ImagePredictor) download(ctx context.Context) error {
 		return errors.New("Need features file checksum in the model manifest")
 	}
 
+	span.LogFields(
+		olog.String("event", "download features"),
+	)
 	if _, err := downloadmanager.DownloadFile(p.GetFeaturesUrl(), p.GetFeaturesPath(), downloadmanager.MD5Sum(checksum)); err != nil {
 		return err
 	}
@@ -162,10 +174,13 @@ func (p *ImagePredictor) download(ctx context.Context) error {
 }
 
 func (p *ImagePredictor) loadPredictor(ctx context.Context) error {
-	if span, newCtx := opentracing.StartSpanFromContext(ctx, "LoadPredictor"); span != nil {
-		ctx = newCtx
-		defer span.Finish()
-	}
+	span, ctx := p.GetTracer().StartSpanFromContext(ctx, "LoadPredictor")
+
+	defer span.Finish()
+
+	span.LogFields(
+		olog.String("event", "read features"),
+	)
 
 	var features []string
 	f, err := os.Open(p.GetFeaturesPath())
@@ -185,6 +200,10 @@ func (p *ImagePredictor) loadPredictor(ctx context.Context) error {
 		return err
 	}
 
+	span.LogFields(
+		olog.String("event", "creating predictor"),
+	)
+
 	pred, err := gocaffe2.New(p.GetGraphPath(), p.GetWeightsPath())
 	if err != nil {
 		return err
@@ -195,16 +214,29 @@ func (p *ImagePredictor) loadPredictor(ctx context.Context) error {
 }
 
 func (p *ImagePredictor) Predict(ctx context.Context, data [][]float32, opts dlframework.PredictionOptions) ([]dlframework.Features, error) {
-	if span, newCtx := opentracing.StartSpanFromContext(ctx, "Predict", opentracing.Tags{
+	span, ctx := p.GetTracer().StartSpanFromContext(ctx, "Predict", opentracing.Tags{
 		"model_name":        p.Model.GetName(),
 		"model_version":     p.Model.GetVersion(),
 		"framework_name":    p.Model.GetFramework().GetName(),
 		"framework_version": p.Model.GetFramework().GetVersion(),
 		"batch_size":        p.BatchSize(),
-	}); span != nil {
-		ctx = newCtx
-		defer span.Finish()
+	})
+
+	if err := p.predictor.StartProfiling("caffe2", "predict"); err == nil {
+		defer func() {
+			p.predictor.EndProfiling()
+			profBuffer, err := p.predictor.ReadProfile()
+			if err != nil {
+				return
+			}
+			if t, err := ctimer.New(profBuffer); err == nil {
+				t.Publish(ctx, p.GetTracer())
+			}
+			p.predictor.DisableProfiling()
+		}()
 	}
+
+	defer span.Finish()
 
 	var input []float32
 	for _, v := range data {
