daa23955b468891592efab9dd21be0c2937255d8
Abdul Dakkak <abduld@wolfram.com>
2018-09-15 20:54:58 -0500

    add download method

--- predict/predict.go
+++ predict/predict.go
@@ -49,6 +49,39 @@ func New(model dlframework.ModelManifest, opts ...options.Option) (common.Predic
 	return predictor.Load(context.Background(), model, opts...)
 }
 
+// Download ...
+func (p *ImagePredictor) Download(ctx context.Context, model dlframework.ModelManifest, opts ...options.Option) error {
+	span, ctx := tracer.StartSpanFromContext(ctx, tracer.STEP_TRACE, "Download")
+	defer span.Finish()
+
+	framework, err := model.ResolveFramework()
+	if err != nil {
+		return err
+	}
+
+	workDir, err := model.WorkDir()
+	if err != nil {
+		return err
+	}
+
+	ip := &ImagePredictor{
+		ImagePredictor: common.ImagePredictor{
+			Base: common.Base{
+				Framework: framework,
+				Model:     model,
+				Options:   options.New(opts...),
+			},
+			WorkDir: workDir,
+		},
+	}
+
+	if err = ip.download(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Load ...
 func (p *ImagePredictor) Load(ctx context.Context, model dlframework.ModelManifest, opts ...options.Option) (common.Predictor, error) {
 	span, ctx := tracer.StartSpanFromContext(ctx, tracer.STEP_TRACE, "Load")
